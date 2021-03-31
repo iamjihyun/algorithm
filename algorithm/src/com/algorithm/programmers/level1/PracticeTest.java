4ca8af579b273065cacf3d6f964d949c8a298546
jihyun <appeldromen@gmail.com>
2021-03-31 21:06:36 +0900

    210328 스터디 진행분 커밋

--- algorithm/src/com/algorithm/programmers/level1/PracticeTest.java
+++ algorithm/src/com/algorithm/programmers/level1/PracticeTest.java
@@ -3,16 +3,17 @@ package com.algorithm.programmers.level1;
 public class PracticeTest {
 //    수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
 //
-//            1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-//            2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
-//            3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+//    1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+//    2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+//    3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
 //
-//            1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+//    1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 //
 //    제한 조건
-//    시험은 최대 10,000 문제로 구성되어있습니다.
-//    문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
-//    가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+//    - 시험은 최대 10,000 문제로 구성되어있습니다.
+//    - 문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+//    - 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+
 //    입출력 예
 //    answers	    return
 //    [1,2,3,4,5]	[1]
@@ -28,25 +29,40 @@ public class PracticeTest {
 //    입출력 예 #2
 //
 //    모든 사람이 2문제씩을 맞췄습니다.
-
     public static void main(String[] args) {
         int[] result = {};
-        result = Solution(new int[]{1, 2, 3, 4, 5});
-        System.out.println("result : " + result);
+        int[] answer = new int[]{1,2,3,4,5,6,7,8,9,10};
+        result = Solution(answer);
+        System.out.println("result : " + result.toString());
     }
 
-    public static int[] Soulution(int[] answers){
+
+    public static int[] Solution(int[] answers){
         int[] answer = {};
         // 문제 수 = 배열 길이
         int answerCnt = answers.length;
         System.out.println("문제 수 : " + answerCnt);
-        int[] one = new int[10000];
-        int[] two = new int[10000];
-        int[] thr = new int[10000];
 
-        for(int i = 1; i <= answerCnt; i++){
+        //문제 수 만큼 배열을 만든다.
+        int[] one = new int[answerCnt];
+        int[] two = new int[answerCnt];
+        int[] thr = new int[answerCnt];
 
+        for(int i = 0; i < answerCnt; i++){
+            one[i] = (i+1) % 5;
+            if (one[i] == 0 ){
+                one[i] = 5;
+            }
+            //짝수자리 1,3,4,5
+            //홀수자리는 모두 2
+            if((i+1) % 2 == 1){
+                two[i] = 2;
+            }else{
+                two[i] = 2;
+            }
+            thr[i] = 0;
 
+            System.out.println("one[" + i + "] : " + one[i]);
         }
 
 
